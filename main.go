64669baf1a14977baf72a7c182774ad7e44182eb
Jakub F. Bortlík <jakub.bortlik@proton.me>
2025-05-07 23:50:26 +0200

    feat: handle readiness

--- main.go
+++ main.go
@@ -9,8 +9,11 @@ func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	fs := http.FileServer(http.Dir(filepathRoot))
+
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app/", fs))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -18,4 +21,10 @@ func main() {
 	}
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
