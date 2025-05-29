4c404ff0d7976e38f08e6e00ab68ecfa9a1215b9
Jakub F. Bortlík <jakub.bortlik@proton.me>
2025-05-29 15:49:19 +0200

    feat: add chirp validation

---
+++ chirps.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func handlerChirpValidation(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body *string `json:"body"`
+	}
+	type returnVals struct {
+		Valid bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong", err)
+		return
+	}
+
+	if params.Body == nil {
+		respondWithError(w, http.StatusBadRequest, "The request body doesn't contain the required field `body`", nil)
+		return
+	}
+	const maxChirpLength = 140
+	if chirp_length := len(*params.Body); chirp_length > maxChirpLength {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, returnVals{
+		Valid: true,
+	})
+	return
+}
--- main.go
+++ main.go
@@ -21,6 +21,7 @@ func main() {
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", fs)))
 
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("POST /api/validate_chirp", handlerChirpValidation)
 
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
